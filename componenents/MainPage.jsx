59306d149e4338041991699a5638f649a0dba309
Vedant Tapkir <47036160+Octaflick@users.noreply.github.com>
2026-04-07 17:39:55 +0530

    Added TextEditor, Basic Styles

--- componenents/MainPage.jsx
+++ componenents/MainPage.jsx
@@ -1,10 +1,11 @@
+import TextEditor from "./TextEditor"
 import { useState } from "react";
 
 export default function MainPage() {
   return (
     <div id="mainpage">
       <p id="quotetop">Quote Top</p>
-      
+      <TextEditor/>
       <p id="quotebottom">Quote Bottom</p>
     </div>
   );
---
+++ componenents/TextEditor.jsx
@@ -0,0 +1,5 @@
+let TextEditor = ()=>{
+    return <textarea res id="texteditor" placeholder="Start Venting.." >TEST</textarea>
+}
+
+export default TextEditor
