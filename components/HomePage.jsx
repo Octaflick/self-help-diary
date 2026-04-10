9b7118245cb7ad27caba3aabeebb1192fbf309a4
Vedant Tapkir <47036160+Octaflick@users.noreply.github.com>
2026-04-10 20:15:38 +0530

    Change: Click me added

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -2,7 +2,7 @@ export default function HomePage({ onStart }) {
 	return (
 		<div id='homepage'>
 			<h1 className='handwriting' id='start-writing' onClick={onStart}>
-				start writing
+				start writing(click me)
 			</h1>
 		</div>
 	);
